220b1a34732ed8f12c8898534ad9b334eb1b2731
Consultant <consultant@Admins-MBP-3.lan>
2023-09-24 16:56:52 -0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+fun main(args: Array<String>) {
+    var mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
+    println("mShoppingList original: $mShoppingList.")
+    val extraShopping = listOf("Cookies", "Sugar", "Eggs")
+    mShoppingList.addAll(extraShopping)
+    println("mShoppingList items added: $mShoppingList.")
+    if (mShoppingList.contains("Tea")){
+        mShoppingList.set(mShoppingList.indexOf("Tea"), "Coffee")
+    }
+    mShoppingList.sort()
+    println("mShoppingList sorted: $mShoppingList")
+    mShoppingList.reverse()
+    println("mShoppingList reversed: $mShoppingList")
+
+    val mShoppingSet = mShoppingList.toMutableSet()
+    println("mShoppingSet: $mShoppingSet.")
+    val moreShopping = setOf("Chives", "Spinach", "Milk")
+    println("mShoppingSet items added: $mShoppingSet")
+    mShoppingList = mShoppingSet.toMutableList()
+    println("mShoppingList new version: $mShoppingList")
+
+    val r1 = Recipe("Chicken Soup")
+    val r2 = Recipe("Quinoa Salad")
+    val r3 = Recipe("Jambalaya")
+    val r4 = Recipe("Sausage Rolls")
+    val r5 = Recipe("Thai Curry")
+    val mRecipeMap = mutableMapOf("Recipe 1" to r1, "Recipe" to r2, "Recipe 3" to r3)
+    println("mRecipeMap original: $mRecipeMap.")
+    val recipesToAdd = mapOf("Recipe 4" to r4, "Recipe 5" to r5)
+    mRecipeMap.putAll(recipesToAdd)
+    println("mRecipeMap updated: $mRecipeMap.")
+    if (mRecipeMap.containsKey("Recipe 1")){
+        println("Recipe 1 is: ${mRecipeMap.getValue("Recipe 1")}")
+    }
+}
